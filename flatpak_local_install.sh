291520f880203ab76103ea4921086ba0087710c9
Eren Seymen <erenseymen@gmail.com>
2025-07-19 03:06:10 +0300

    fix: remove redundant Flathub installation command from flatpak_local_install.sh

--- flatpak_local_install.sh
+++ flatpak_local_install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Install Flathub
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
 # Build & install
-flatpak-builder --force-clean --user --install-deps-from=flathub --install build-dir flatpak-manifest.json
+flatpak-builder --force-clean --install-deps-from=flathub --install build-dir flatpak-manifest.json
 
 # Run
 flatpak run com.example.RunNTimes
